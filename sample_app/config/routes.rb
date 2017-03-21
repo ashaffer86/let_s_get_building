75ee927482c75d46208d9da01010b523f0da0918
ashaffermyers <andrewshaffer1986@gmail.com>
2017-03-20 22:38:17 -0500

    Finish Layout and routes

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -2,10 +2,36 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+
+  #1Refer to these to know exactly how routing works in the background.
+  #the equivalent of resources: :posts
+  #######
+  #get "/posts" => "posts#index"
+  #get "/posts/:id" => "posts#show"
+  #get "/posts/new" => "posts#new"
+  #post "/posts" => "posts#create"  # usually a submitted form
+  #get "/posts/:id/edit" => "posts#edit"
+  #put "/posts/:id" => "posts#update" # usually a submitted form
+  #delete "/posts/:id" => "posts#destroy"
+  ######Advantage     - You have more explicit control over the routes to controllers
+  ######              - Useful if you aren't exactly following RESTful convention
+  ######Disadvantage  - More typing
+
+  #2 If I want to leave out options for my "RESTful" resource, use this syntax
+  ############
+  # resources :posts, :only => [:index, :show]
+  #  resources :users, :except => [:index]
+  #################
+
+  #3To replace "show" logic for a logged in user context, do this:
+  ######get 'profile', to: 'users#show'
+  #means - http get request using "profile" uri should direct them to "users#show"
+  #let users#show figure out the individual that is logged in.
+  #############################
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
