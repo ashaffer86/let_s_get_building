c22f64c094b75770540a3d8b59d945b60407457f
ashaffermyers <andrewshaffer1986@gmail.com>
2017-04-02 21:51:23 -0500

    Implement basic login - Chapter 8

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -2,14 +2,18 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get '/help',      to: 'static_pages#help'
+  get '/about',     to: 'static_pages#about'
+  get '/contact',   to: 'static_pages#contact'
+
+  get '/signup',    to: 'users#new'
+  post '/signup',   to: 'users#create'
 
   resources :users, only: [:index, :edit, :show, :update, :destroy]
 
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   #get "/users", to: 'users#index'
   #post "/users", to: 'users#create'
