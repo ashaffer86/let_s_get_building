a7b9b4ad281949a6ed99e9425ffaa45bbc259773
ashaffermyers <andrewshaffer1986@gmail.com>
2017-04-16 16:14:39 -0500

    Chapter 11, right before email preview tests

---
+++ sample_app (copy)/test/integration/users_signup_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "Find Signup Page" do
+    get signup_path
+    assert_template "users/new"
+  end
+
+  test "Invalid Form Submission" do
+
+    assert_no_difference "User.count" do
+      get signup_path
+      post signup_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+        }
+      }
+    end
+    assert_template "users/new"
+    assert_select("div#error_explanation")
+    assert_select("div.field_with_errors")
+  end
+
+  test "Valid Form Submission" do
+    assert_difference "User.count" do
+      get signup_path
+      post signup_path, params: {
+        user: {
+          name: "r_Test User",
+          email: "r_test@somewhere.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert is_logged_in?
+  end
+end
